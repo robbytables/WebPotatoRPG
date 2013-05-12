079c3d6b79645e8a7353ff3570dd3d4b5b0e1ae9
redpancho <redpancho@gmail.com>
2013-05-12 19:11:50 -0400

    app test

--- application/controllers/potato.php
+++ application/controllers/potato.php
@@ -32,12 +32,11 @@ class Potato_Controller extends Base_Controller {
     public function action_index() {
         $count = DB::table('users')->count();
         $fart = "what";
-//        $data = array(
-//            'totalUsers' => $count,
-//            'name' => $fart
-//        );
-        $data = $count;
-        
+        $data = array(
+            'totalUsers' => $count,
+            'name' => $fart
+        );
+
         return View::make('potato.potato', $data);
     }
 
