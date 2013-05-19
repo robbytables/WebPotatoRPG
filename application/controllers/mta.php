f8f50a713b2377cc66737df86a67cdc4486e9ffb
redpancho <redpancho@gmail.com>
2013-05-19 18:42:51 -0400

    adjust parsed output

--- application/controllers/mta.php
+++ application/controllers/mta.php
@@ -9,8 +9,9 @@ class MTA_Controller extends Base_Controller {
 
          // protoc -I tmp/ tmp/nyct-subway.proto --decode=transit_realtime.FeedMessage > tmp/decodedmtafeed2
 
-        $parsedMta = shell_exec('cat '.$mta.'.txt | protoc -I public/data/mapping public/data/mapping/nyct-subway.proto --decode=transit_realtime.FeedMessage > '.$mta.'parsed.txt');
+        $parsedMta = shell_exec('cat '.$mta.'.txt | protoc -I public/data/mapping public/data/mapping/nyct-subway.proto --decode=transit_realtime.FeedMessage');
 
+        File::put('public/data/mtaparsed.txt', $mta);
 
         return View::make('mta.index');
     }
