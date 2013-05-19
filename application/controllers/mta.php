19a37349feb412496663c4ab930ba17e04a87f60
redpancho <redpancho@gmail.com>
2013-05-19 17:39:24 -0400

    get the new file and write it

--- application/controllers/mta.php
+++ application/controllers/mta.php
@@ -3,11 +3,10 @@
 class MTA_Controller extends Base_Controller {
 
     public function action_index() {
-        $data = array(
-            'totalUsers' => $count = DB::table('users')->count()
-        );
+        $mta = file_get_contents('http://datamine.mta.info/mta_esi.php?key=7073cdec69d4429e24d25b6bbc3e91bc');
+        File::put('public/data/mta.txt', $mta);
 
-        return View::make('mta.index')->with('data', $data);
+        return View::make('mta.index');
     }
 
 }
