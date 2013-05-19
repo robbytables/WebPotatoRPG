f2e2e9cd3edc205fd9da39e32e69eafca42ccc8b
redpancho <redpancho@gmail.com>
2013-05-19 18:51:04 -0400

    fixit\!

--- application/controllers/mta.php
+++ application/controllers/mta.php
@@ -9,8 +9,9 @@ class MTA_Controller extends Base_Controller {
 
          // protoc -I tmp/ tmp/nyct-subway.proto --decode=transit_realtime.FeedMessage > tmp/decodedmtafeed2
 
-        $parsedMta = shell_exec('cat public/data/mta.txt | protoc -I public/data/mapping public/data/mapping/nyct-subway.proto --decode=transit_realtime.FeedMessage');
-        echo getcwd() . "\n";
+        $parsedMta = shell_exec('cat public/data/mta.txt | protoc -I public/data/mapping/proto public/data/mapping/proto/nyct-subway.proto --decode=transit_realtime.FeedMessage');
+
+
         echo $parsedMta;
 
         File::put('public/data/mtaparsed.txt', $parsedMta);
