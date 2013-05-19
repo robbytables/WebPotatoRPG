62bebf1c5c9afff848c077e423c2754762967c68
redpancho <redpancho@gmail.com>
2013-05-19 18:38:42 -0400

    updating controller

--- application/controllers/mta.php
+++ application/controllers/mta.php
@@ -4,7 +4,13 @@ class MTA_Controller extends Base_Controller {
 
     public function action_index() {
         $mta = file_get_contents('http://datamine.mta.info/mta_esi.php?key=7073cdec69d4429e24d25b6bbc3e91bc');
+
         File::put('public/data/mta.txt', $mta);
+
+         // protoc -I tmp/ tmp/nyct-subway.proto --decode=transit_realtime.FeedMessage > tmp/decodedmtafeed2
+
+        $parsedMta = shell_exec('cat '.$mta.'.txt | protoc -I public/data/mapping public/data/mapping/nyct-subway.proto --decode=transit_realtime.FeedMessage > '.$mta.'parsed.txt');
+
 
         return View::make('mta.index');
     }
