3bcf45821958daaded9a78a7a4159118d38319b1
redpancho <redpancho@gmail.com>
2013-05-19 20:00:30 -0400

    test again

--- application/views/mta/index.blade.php
+++ application/views/mta/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>MTA Trips</h1>
     <h3>{{ date('h:i:sa', $data['currentTime']) }}</h3>
+    <h2> {{ $data['currentTime'] }} </h2>
     <span data-livestamp="{{ $data['currentTime'] }}"></span>
     <div id="test"></div>
     <a id="copyright" href="https://twitter.com/chris_germano">Chris Germano 2013</a>
