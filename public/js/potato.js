6bde92b200011adf75eaa8d5b8377cb648003c40
Chris Germano <germano.c@husky.neu.edu>
2013-05-21 19:12:24 -0400

    Ninja commit

--- public/js/potato.js
+++ public/js/potato.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
 					url: "potato/dbGetEvent",
 					data: {_name: name}
 				}).done(function(data) {
-					eventText = data.event[1].replace("+name+",data.name);
+					eventText = data.event[1].replace("+name+",name);
 					document.getElementById("eventText").innerHTML = eventText;
 					setChoices(data.event[2]);
 					console.log(data);
