0c462bbbcd539fd741ff2320e122469f8d20de64
Chris Germano <germano.c@husky.neu.edu>
2013-05-21 12:48:37 -0400

    Favicon fix

--- application/views/potato/base.blade.php
+++ application/views/potato/base.blade.php
@@ -3,6 +3,7 @@
   <head>
 	<meta charset="utf-8">
 	<title>Potato RPG</title>
+	<link rel="icon" type="image/ico" href="favicon.ico"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
